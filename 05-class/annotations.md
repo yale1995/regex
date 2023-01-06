1ef2b156ee47472bcfc436e9b6efb77637d17607
Yale Araujo <yale850@gmail.com>
2023-01-06 10:32:16 -0300

    class 05

---
+++ 05-class/annotations.md
@@ -0,0 +1,14 @@
+Some flags useds in RegEx
+
+^  => Negative from set
+
+[abc]      => set of caracters
+[min-max]  => optional 0 or 1 {0,1}
+[^min-max] => everything except this set 
+
+\d => just numbers
+\D => any caractere execpt numbers
+\s => blank espaces
+\S => any alpha numeric caracter, except blank space
+\w => any aplha numeric caracter, incluind underline
+\W => any caractere except alpha numerics and underline
---
+++ 05-class/index.js
@@ -0,0 +1,26 @@
+const {alfabeto} = require('../base');
+
+// console.log(alfabeto)
+// console.log(alfabeto.match(/[0-9]/))
+// console.log(alfabeto.match(/[0-9]/g))
+// console.log(alfabeto.match(/[0-9]+/g))
+
+// console.log(alfabeto.match(/[a-z]/g))
+// console.log(alfabeto.match(/[a-z]/gi))
+// console.log(alfabeto.match(/[a-z]+/g))
+
+// console.log(alfabeto.match(/[^a-z]+/g))
+// console.log(alfabeto.match(/[^a-z]+/gi))
+
+// console.log(alfabeto.match(/[^a-zA-Z0-9]+/g))
+
+// console.log(alfabeto.match(/\w/g))
+// console.log(alfabeto.match(/\W/g))
+// console.log(alfabeto.match(/\w+/g))
+// console.log(alfabeto.match(/\W+/g))
+
+// console.log(alfabeto.match(/\d+/g))
+// console.log(alfabeto.match(/\D+/g))
+
+// console.log(alfabeto.match(/\s+/g))
+// console.log(alfabeto.match(/\S+/g))
